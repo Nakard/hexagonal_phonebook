78296ec360fb5ee6628c2127cc538d6ff2e6be3a
Arkadiusz Moskwa <a.moskwa@gmail.com>
2015-08-21 20:58:48 +0200

    add user bundle add fos rest bundle

---
+++ src/Bundle/UserBundle/ArkonUserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Arkon\Bundle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ArkonUserBundle
+ * @package Arkon\Bundle\UserBundle
+ */
+class ArkonUserBundle extends Bundle
+{
+}
